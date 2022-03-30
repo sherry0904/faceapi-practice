27508ec894ac9ba2ccc48b0d720c1f9eb0643e13
Sherry <sherryh.hsieh@TWTAADO201101.local>
2022-03-30 12:56:18 +0800

    nose position

--- js/detect.js
+++ js/detect.js
@@ -75,8 +75,8 @@ detect = function () {
 
       let noseOffsetW = $(".rednose").width() / 2;
 
-      $(".rednose").css("top",_faceLandmarks.positions[33].y - noseOffsetW + "px");
-      $(".rednose").css("left",_faceLandmarks.positions[33].x - noseOffsetW + "px");
+      $(".rednose").css("top",_faceLandmarks.positions[31].y - noseOffsetW + "px");
+      $(".rednose").css("left",_faceLandmarks.positions[31].x - noseOffsetW + "px");
     }
 
     //FaceExpressions
